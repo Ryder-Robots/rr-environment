ed825595100b7ad24278158701423a38c02feb27
ryderrobots@gmail.com <azzmosphere@gmail.com>
2024-07-06 17:46:20 +1000

    added initial build files

---
+++ include/rrenvironment/AbstractAction.hpp
@@ -0,0 +1,12 @@
+/************************************************
+ * General interface to interact with actions.
+ ************************************************/
+
+#ifndef ABSTRACTACTION_HPP
+#define ABSTRACTACTION_HPP
+
+#include <string>
+
+
+
+#endif
