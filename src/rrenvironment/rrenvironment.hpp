dbdb915b1820ac6df75cbe9fdce45b750a68d1c9
ryderrobots@gmail.com <azzmosphere@gmail.com>
2024-07-08 22:26:12 +1000

    * Added rrenvironment header.

--- src/rrenvironment/rrenvironment.hpp
+++ src/rrenvironment/rrenvironment.hpp
@@ -8,20 +8,32 @@
 #include <string>
 #include <map>
 
+#include "dlib/threads.h"
+#include "nlohmann/json.hpp"
+
 using namespace std;
+using json = nlohmann::json;
 
 class RrEnvironment {
     public:
+        void setup_threadpool(dlib::thread_pool& tp);
+
         /*
          * Sets hardware observation class.
          */
         void add_hw_observer(const unsigned int oid, const string hw_class, const map<std::string, int>&  config);
 
         /*
-        * set hw action and add it to list.
+         * set hw action and add it to list.
          */
         void add_hw_action(const unsigned int oid, const string hw_class, const map<std::string, int>&  config);
 
+        /*
+         * Execute actions.
+         */
+        void execute_actions(const dlib::future<map<std::string, json>> args);
+
+        void get_observations(const dlib::future<map<std::string, json>> observations);
 };
 
 #endif
