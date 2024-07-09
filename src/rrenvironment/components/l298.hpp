1e4456e0f3e86d417bc1df96667a3bb555361785
ryderrobots@gmail.com <azzmosphere@gmail.com>
2024-07-09 17:14:54 +1000

    added more code

--- src/rrenvironment/components/l298.hpp
+++ src/rrenvironment/components/l298.hpp
@@ -5,9 +5,10 @@
 #ifndef L298
 #define L298
 
-#include <string.h>
+#include <string>
+#include <map>
 #include <dlib/logger.h>
-#include "component.hpp"
+#include "rrenvironment/components/component.hpp"
 
 #define MAX_PWM 1024
 
@@ -18,10 +19,12 @@ const std::string IN4 = "IN4";
 const std::string ENA = "ENA";
 const std::string ENB = "ENB";
 
+using namespace std;
+
 class L298 : public Component {
 
     public:
-        void setup(json& config) {
+        void setup(const map<std::string, int>& config) {
             
             dlog << LINFO << "configuring L298";
 
@@ -36,7 +39,7 @@ class L298 : public Component {
             dlog << LINFO << "finished configuring L298";
         }
 
-        void run(json& action_io) {
+        void run(const map<std::string, int>&  action_io) {
             try {
                 digitalWrite(_in1, get_config_value(IN1, action_io));
                 digitalWrite(_in2, get_config_value(IN2, action_io));
@@ -64,7 +67,7 @@ class L298 : public Component {
         unsigned int _ena;
         unsigned int _enb;
 
-        unsigned int get_config_value(const string key, const json config) {
+        unsigned int get_config_value(const string key, const map<std::string, int>&  config) {
             if (config[key].is_number() && (int) config[key] > -1) {
                 dlog << LFATAL << "unable to L298 missing key '" << key << "'";
                 throw 1;
@@ -72,7 +75,7 @@ class L298 : public Component {
             return (unsigned int) config[key];
         }
 
-        unsigned int get_config_value_pwm(const string key, const json config) {
+        unsigned int get_config_value_pwm(const string key, const map<std::string, int>&  config) {
             unsigned val = get_config_value(key, config);
 
             if (val > MAX_PWM) {
--- src/rrenvironment/rrenvironment.cpp
+++ src/rrenvironment/rrenvironment.cpp
@@ -1,7 +1,2 @@
 #include "rrenvironment.hpp"
 
-
-/*******************************************************************************
- * Create indivual hardware object
- *******************************************************************************/
-// void RrEnvironment::setup_hw(const unsigned int oid, const string hw_class, const json config) {
