795f277efec27b3fae7e7bf8c45f9bec82bcba21
Aaron Spiteri <azzmosphere@gmail.com>
2024-07-31 20:05:13 +1000

    minor changes to docker file

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-PASSWORD=${1}
+USERNAME=${1}
+PASSWORD=${2}
 
 apt-get install -y git sudo openssh-server cmake gcc g++ gdb bzip2 \
   libopenblas-dev liblapack-dev ffmpeg pkg-config \
@@ -11,25 +12,27 @@ apt-get install -y git sudo openssh-server cmake gcc g++ gdb bzip2 \
 
 apt-get install -y /tmp/wiringpi_3.6_arm64.deb
 
+
 # Set up git username
-git config --global user.name "Mona Lisa"
+git config --global user.name "Aaron SPiteri"
 git config --global user.email "azzmosphere@gmail.com"
 
-useradd -p ${PASSWORD} -m aaron
-usermod -aG sudo aaron
-chown aaron /tmp/id_rsa 
-su aaron -c "mkdir /home/aaron/.ssh"
-su aaron -c "cp /tmp/config /home/aaron/.ssh/"
-su aaron -c "cp /tmp/id_rsa  /home/aaron/.ssh/"
-su aaron -c "cp /tmp/id_rsa.pub /home/aaron/.ssh/"
-su aaron -c "chmod -R 700  /home/aaron/.ssh"
-su aaron -c "git config --global user.name 'Aaron Spiteri' && git config --global user.email 'azzmosphere@gmail.com.au'"
-su aaron -c "cd /home/aaron && git clone git@github.com:Ryder-Robots/rr-environment.git"
+useradd -m -p $(echo "${PASSWORD}" | openssl passwd -1 -stdin) ${USERNAME} > /dev/null 2>&1
+usermod -aG sudo ${USERNAME}
 
-su aaron -c "cd /home/aaron && \
+chown ${USERNAME} /tmp/id_rsa 
+su ${USERNAME} -c "mkdir /home/aaron/.ssh"
+su ${USERNAME} -c "cp /tmp/config /home/aaron/.ssh/"
+su ${USERNAME} -c "cp /tmp/id_rsa  /home/aaron/.ssh/"
+su ${USERNAME} -c "cp /tmp/id_rsa.pub /home/aaron/.ssh/"
+su ${USERNAME} -c "chmod -R 700  /home/aaron/.ssh"
+su ${USERNAME} -c "git config --global user.name 'Aaron Spiteri' && git config --global user.email 'azzmosphere@gmail.com.au'"
+su ${USERNAME} -c "cd /home/aaron && git clone git@github.com:Ryder-Robots/rr-environment.git"
+
+su ${USERNAME} -c "cd /home/aaron && \
   git clone https://github.com/davisking/dlib.git &&
   cd dlib && mkdir build; cd build; cmake .. ; cmake --build ."
-cd /home/aaron/dlib/build && cmake --install .
+cd /home/${USERNAME}/dlib/build && cmake --install .
 
 # Setup VSCODE extensions
 for ex in crugthew.c-cpp-linter \
@@ -40,6 +43,6 @@ for ex in crugthew.c-cpp-linter \
   ms-vscode.cpptools-themes \
   twxs.cmake \
   vadimcn.vscode-lldb; do
-  su aaron -c "code  --install-extension ${ex}"
+  su ${USERNAME} -c "code  --install-extension ${ex}"
 done
 
